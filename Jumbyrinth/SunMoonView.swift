e69ea60f90b82f38bce9086801d2a2c0ff65537c
ShenghaoLin <sl4549@nyu.edu>
2017-11-29 01:44:56 -0500

    weather loaded; day/night can be detected; still need to find icons representing different weathers

--- Jumbyrinth/SunMoonView.swift
+++ Jumbyrinth/SunMoonView.swift
@@ -18,7 +18,6 @@ class SunMoonView: UIView, CLLocationManagerDelegate {
     
     var imageWidth : CGFloat = 80
     var imageHeight : CGFloat = 80
-    
     var imageView = UIImageView.init(frame: CGRect(x: 0, y: 0, width: 80, height: 80))
     
     var currentPoint = CGPoint()
@@ -34,8 +33,35 @@ class SunMoonView: UIView, CLLocationManagerDelegate {
         fatalError("init(coder:) has not been implemented")
     }
 
+    private func loadWeather(data : Data) {
+        
+        
+        do {
+            let json = try JSONSerialization.jsonObject(with: data, options: []) as! NSDictionary
+            let sys = json["sys"] as! [String:AnyObject]
+            let sunset = Date(timeIntervalSince1970: sys["sunset"] as! Double)
+            let sunrise = Date(timeIntervalSince1970: sys["sunrise"] as! Double)
+            
+            if ((sunrise.compare(Date()) == ComparisonResult.orderedAscending)&&(sunset.compare(Date()) == ComparisonResult.orderedDescending)) {
+                self.imageView.image = UIImage.init(named: "Sun");
+            }
+            else {
+                self.imageView.image = UIImage.init(named: "Moon");
+            }
+            
+            //TODO: change icon based on the weather
+            
+        }
+        catch {
+            print("no data loaded")
+        }
+       
+    }
+    
     
     private func setupUI() {
+        
+        self.imageView.image = UIImage.init(named: "Sun");
         
         manager = CLLocationManager()
         manager.delegate = self
@@ -43,10 +69,15 @@ class SunMoonView: UIView, CLLocationManagerDelegate {
         manager.requestWhenInUseAuthorization()
         manager.startUpdatingLocation()
         
+        let loc = manager.location?.coordinate
         
+        let weatherUrl = URL(string: String(format: "https://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&APPID=705a4acb14b320b3657d6cd1e9206db6", Double((loc?.latitude)!), Double((loc?.longitude)!)))
         
-        
-        imageView.image = UIImage.init(named: "Sun")
+        do {
+            let data = try Data(contentsOf: weatherUrl!)
+            self.loadWeather(data: data)
+        }
+        catch {}
         
         let date = Date()
         let calendar = Calendar.current
