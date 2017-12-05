2620e4f500942702f92d3cb2ca1631faec694e01
ShenghaoLin <sl4549@nyu.edu>
2017-12-05 12:58:55 -0500

    fix the bug: unstable colorc change spped

--- Jumbyrinth/ColorfulView.swift
+++ Jumbyrinth/ColorfulView.swift
@@ -16,38 +16,41 @@ class ColorfulView: UIView {
         static var green = 0.67
         static var blue = 0.90
         
-        
         static var now = "Blue"
     }
     
     var timer = Timer()
     
     func runTimer() {
+        
+        timer.invalidate()
         timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
+        
     }
     
     
     @objc func updateTimer() {
         
         if (Static.now == "Blue") {
-            Static.blue += 0.0001
-            Static.red -= 0.00005
-            Static.green -= 0.00005
+            Static.blue += 0.0009
+            Static.red -= 0.0006
+            Static.green -= 0.0003
         }
         if (Static.now == "Red") {
-            Static.blue -= 0.00005
-            Static.red += 0.0001
-            Static.green -= 0.00005
+            Static.blue -= 0.0003
+            Static.red += 0.0009
+            Static.green -= 0.0006
         }
         if (Static.now == "Green") {
-            Static.blue -= 0.00005
-            Static.red -= 0.00005
-            Static.green += 0.0001
+            Static.blue -= 0.0006
+            Static.red -= 0.0003
+            Static.green += 0.0009
         }
         
         if (Static.red > 0.95) {
             Static.red = 0.95
             Static.now = "Blue"
+            print("Blue")
         }
         if (Static.red < 0.05) {
             Static.red = 0.05
@@ -56,6 +59,7 @@ class ColorfulView: UIView {
         if (Static.green > 0.95) {
             Static.green = 0.95
             Static.now = "Red"
+            print("Red")
         }
         if (Static.green < 0.05) {
             Static.green = 0.05
@@ -64,6 +68,7 @@ class ColorfulView: UIView {
         if (Static.blue > 0.95) {
             Static.blue = 0.95
             Static.now = "Green"
+            print("Green")
         }
         if (Static.blue < 0.05) {
             Static.blue = 0.05
