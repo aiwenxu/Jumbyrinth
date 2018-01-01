5b105ca9ffc73b6eaa45da56703d4e30f2b17347
ShenghaoLin <sl4549@nyu.edu>
2017-12-31 21:39:43 -0800

    delete use of location; adjust widget positions

--- Jumbyrinth/SunMoonView.swift
+++ Jumbyrinth/SunMoonView.swift
@@ -11,10 +11,9 @@ import UIKit
 import CoreLocation
 
 // The class designed to control the sun/moon icon on the buttom of the screen
-class SunMoonView: UIView, CLLocationManagerDelegate {
+class SunMoonView: UIView {
     
     // Location service used to get the location and further to read current weather info in this place
-    var manager:CLLocationManager!
     
     var imageWidth : CGFloat = 80
     var imageHeight : CGFloat = 80
@@ -35,29 +34,7 @@ class SunMoonView: UIView, CLLocationManagerDelegate {
     // The method using given weather data from url to draw sun/moon icon
     private func loadWeather(data : Data) {
         
-        // try to read the data
-        do {
-            //read data of JSON type
-            let json = try JSONSerialization.jsonObject(with: data, options: []) as! NSDictionary
-            let sys = json["sys"] as! [String:AnyObject]
-            
-            //get sunset and sunrise time in the format of Date
-            let sunset = Date(timeIntervalSince1970: sys["sunset"] as! Double)
-            let sunrise = Date(timeIntervalSince1970: sys["sunrise"] as! Double)
-            
-            drawSunAndMoon(sunrise: sunrise, sunset: sunset)
-            
-            //TODO: change icon and game mode based on the weather
-        }
-            
-        // if data cannot be read, there is error happening during getting data from the website
-        // in this case, we use the default sunrise and sunset setting to draw the icon
-        catch {
-            
-            drawSunAndMoon(sunrise: defaultSunrise(), sunset: defaultSunset())
-            print("no data loaded")
-        }
-       
+        drawSunAndMoon(sunrise: defaultSunrise(), sunset: defaultSunset())
     }
     
     //The actual drawing methond the sun/moon icon
@@ -112,46 +89,9 @@ class SunMoonView: UIView, CLLocationManagerDelegate {
     func setupUI() {
         
         currentPoint = CGPoint(x: -100, y: bounds.maxY/2)
-    
-        //try to get the aurthority to get the current location
-        manager = CLLocationManager()
-        manager.delegate = self
-        manager.desiredAccuracy = kCLLocationAccuracyBest
-        manager.requestWhenInUseAuthorization()
-        manager.startUpdatingLocation()
-        
-        //read the location coordianates
-        let loc = manager.location?.coordinate
-        
-        //when the location information is correctly read
-        if (loc != nil) {
-            
-            do {
-                
-                //load data from openweathermap.org using the location info
-                let weatherUrl = URL(string: String(format: "https://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&APPID=705a4acb14b320b3657d6cd1e9206db6", Double((loc?.latitude)!), Double((loc?.longitude)!)))
-                let data = try Data(contentsOf: weatherUrl!)
-                
-                //actual drawing
-                self.loadWeather(data: data)
-            }
-                
-            //when there is no weather information read, using the default sunrise and sunset time
-            catch {
-                drawSunAndMoon(sunrise: defaultSunrise(), sunset: defaultSunset())
-                print("no weather data loaded")
-            }
-        }
-            
-        //when there is no location information read, using the default sunrise and sunset time
-        else {
-            drawSunAndMoon(sunrise: defaultSunrise(), sunset: defaultSunset())
-            print("no location data loaded")
-        }
+        drawSunAndMoon(sunrise: defaultSunrise(), sunset: defaultSunset())
         
         addSubview(self.imageView)
         self.imageView.center = self.currentPoint
-        
-        manager.stopUpdatingLocation()
     }
 }
